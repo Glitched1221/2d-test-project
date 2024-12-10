d4ec0e0054f012e49aae1464c721d9b51b7d7366
Glitched1221 <leostevens008@gmail.com>
2024-12-10 16:25:50 +0000

    Add ai

---
+++ Assets/BasicEnemy.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BasicEnemy : MonoBehaviour
+{
+
+    NavMeshAgent agent;
+    GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+        player = GameObject.FindWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(player.transform.position);
+    }
+}
---
+++ Assets/amooui.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class amooui : MonoBehaviour
+{
+    public Rigidbody player;
+     public Slider slider;
+     Image Amoo;
+     public float maxClipSize = 10;
+     public float currentClip;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Amoo = GetComponent<Image>();
+        
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Amoo.fillAmount = currentClip/maxClipSize;
+    }
+}
